10f09a810c8d2559b0f184b5ba5a5a863a54c5da
DuminduTraa <dumindutissera@gmail.com>
2018-11-01 14:23:25 +0530

    update

--- video_sampler.py
+++ video_sampler.py
@@ -1,14 +1,33 @@
 import numpy as np
 import cv2
 
-time_interval = 5  # time interval between samples in seconds
+import argparse
+ 
+parser = argparse.ArgumentParser(description="Video Sampler")
+    
+    
+parser.add_argument("--input_vid", required=True, help="path to video")
+parser.add_argument("--output_dir", required=True, help="output path")
+parser.add_argument("--time", type=int, default=5, help="time interval between samples in seconds")
 
-cap = cv2.VideoCapture('input/vid9.mp4')
+args = parser.parse_args()
+print(args)
+
+input_dir = args.input_dir
+output_dir = args.output_dir
+height = args.height
+width = args.width
+
+vid_file = args.input_vid
+out_path = args.output_dir
+time_interval = args.time  # time interval between samples in seconds
+
+cap = cv2.VideoCapture(vid_file)
 fps = int(round(cap.get(cv2.CAP_PROP_FPS)))
 #print(fps)
 count = 0
-cap_count = 1383
-out_path = "/home/dumindu/Codes/Video-Sampler/output"
+cap_count = 1
+
 
 while(cap.isOpened()):
     # Capture frame-by-frame
@@ -18,7 +37,7 @@ while(cap.isOpened()):
 
         if count % (time_interval*fps) == 0 : 
             #print(count)
-            cv2.imwrite("output/%d.jpg" % cap_count, frame)
+            cv2.imwrite(out_path + "/%d.jpg" % cap_count, frame)
             cap_count += 1 
 
         # Display the resulting frame
